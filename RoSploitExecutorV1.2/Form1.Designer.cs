ddf79559c78b9b58a31d5be2eef1d7621cc352a6
dumbguy4 <m.umutcakir@gmail.com>
2024-10-09 18:12:54 -0400

    Add files via upload

---
+++ RoSploitExecutorV1.2/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace RoSploitExecutorV1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.robloxopen = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.FunkyFridaySB = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.BedwarsSB = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Execute";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Inject";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(260, 400);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 38);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(384, 400);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 38);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Kill Roblox";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // fastColoredTextBox1
+            // 
+            this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(203, 14);
+            this.fastColoredTextBox1.BackBrush = null;
+            this.fastColoredTextBox1.CharHeight = 14;
+            this.fastColoredTextBox1.CharWidth = 8;
+            this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fastColoredTextBox1.Font = new System.Drawing.Font("Courier New", 9.75F);
+            this.fastColoredTextBox1.IsReplaceMode = false;
+            this.fastColoredTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+            this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fastColoredTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fastColoredTextBox1.ServiceColors")));
+            this.fastColoredTextBox1.Size = new System.Drawing.Size(776, 363);
+            this.fastColoredTextBox1.TabIndex = 5;
+            this.fastColoredTextBox1.Text = "-- Made by random dude";
+            this.fastColoredTextBox1.Zoom = 100;
+            this.fastColoredTextBox1.Load += new System.EventHandler(this.fastColoredTextBox1_Load);
+            // 
+            // robloxopen
+            // 
+            this.robloxopen.AutoSize = true;
+            this.robloxopen.Location = new System.Drawing.Point(539, 381);
+            this.robloxopen.Name = "robloxopen";
+            this.robloxopen.Size = new System.Drawing.Size(69, 13);
+            this.robloxopen.TabIndex = 6;
+            this.robloxopen.Text = "Roblox Open";
+            this.robloxopen.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(539, 394);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(37, 13);
+            this.status.TabIndex = 7;
+            this.status.Text = "Status";
+            this.status.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(539, 407);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Made By Random Dude";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(539, 420);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "72% UNC";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(23, 377);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 21);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Infinite Yield";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // FunkyFridaySB
+            // 
+            this.FunkyFridaySB.Location = new System.Drawing.Point(119, 377);
+            this.FunkyFridaySB.Name = "FunkyFridaySB";
+            this.FunkyFridaySB.Size = new System.Drawing.Size(90, 21);
+            this.FunkyFridaySB.TabIndex = 11;
+            this.FunkyFridaySB.Text = "Funky Friday";
+            this.FunkyFridaySB.UseVisualStyleBackColor = true;
+            this.FunkyFridaySB.Click += new System.EventHandler(this.FunkyFridaySB_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(215, 377);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 21);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Fly Script";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(311, 377);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(90, 21);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Slap Battles Script";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(853, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Script Section";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(407, 377);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(90, 21);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "Piggy Script #1";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // BedwarsSB
+            // 
+            this.BedwarsSB.Location = new System.Drawing.Point(794, 28);
+            this.BedwarsSB.Name = "BedwarsSB";
+            this.BedwarsSB.Size = new System.Drawing.Size(90, 21);
+            this.BedwarsSB.TabIndex = 16;
+            this.BedwarsSB.Text = "Bedwars";
+            this.BedwarsSB.UseVisualStyleBackColor = true;
+            this.BedwarsSB.Click += new System.EventHandler(this.BedwarsSB_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(890, 28);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(90, 21);
+            this.button9.TabIndex = 17;
+            this.button9.Text = "Slap Royale";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(794, 55);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(90, 21);
+            this.button10.TabIndex = 18;
+            this.button10.Text = "Mineblox";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 450);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.BedwarsSB);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.FunkyFridaySB);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.robloxopen);
+            this.Controls.Add(this.fastColoredTextBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "RoSploit Executor V1.2";
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
+        private System.Windows.Forms.Label robloxopen;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button FunkyFridaySB;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button BedwarsSB;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
